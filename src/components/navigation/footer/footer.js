d5e1d500a422558e199effe3caade23c11098066
msikora <marsik98@gmail.com>
2020-03-30 03:57:32 +0300

    NEW footer component

---
+++ src/components/navigation/footer/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './footer.scss'
+
+export const Footer = () => {
+    return <div className="footer-basic">
+        <Footer>
+            <p className="copyright">PIZZA © 2020</p>
+        </Footer>
+    </div>
+};
